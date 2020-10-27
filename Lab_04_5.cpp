92d0920dc8ebc5b2746b67ca6d9749c1656d7f6f
Maksym Danyliuk <71183230+CheapyD@users.noreply.github.com>
2020-10-27 22:37:41 +0200

    Lab_04_5 first commit

---
+++ Lab_04_5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	double x, y, R = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "R = "; cin >> R;
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+
+		if ((y >= (sqrt((R * R) - (x + R) * (x + R))) - R && x <= 0 && y <= 0) ||
+			(y <= sqrt((2 * x * R) - (x * x))))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "R = "; cin >> R;
+
+		x = -2 * R + (2 * R - (-2 * R)) * rand() / RAND_MAX;
+		y = -2 * R + (2 * R - (-2 * R)) * rand() / RAND_MAX;
+
+		if ((y >= (sqrt((R * R) - (x + R) * (x + R))) - R && x <= 0 && y <= 0) ||
+			(y <= sqrt((2 * x * R) - (x * x))))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	
+	system("pause");
+	return 0;
+}
